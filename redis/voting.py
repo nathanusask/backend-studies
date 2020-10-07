6e5aac6d5908d0f3507bb382712e92e13838dae4
Nathan Yang <nathan.yang@usask.ca>
2020-10-07 15:54:06 -0600

    partition -> split

--- redis/voting.py
+++ redis/voting.py
@@ -9,7 +9,7 @@ def article_vote(conn, user, article):
     cutoff = time.time() - ONE_WEEK_IN_SECONDS
     if conn.zscore('time:', article) < cutoff:
         return
-    article_id = article.partition(':')[-1]
+    article_id = article.split(':')[-1]
     if conn.sadd('votrd:' + article_id, user):
         conn.zincrby('score:', article, VOTE_SCORE)
         conn.hincrby(article, 'votes', 1)
